62b4e7859f6944345a7164dda8441045daa18651
Ronaldo Kwan <Ronaldo.Kwan@student.uts.edu.au>
2026-07-12 11:27:57 +1000

    Update CI configuration and Playwright reporter settings for improved compatibility and reporting

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  reporter: process.env.CI ? 'line' : 'list',
+  reporter: process.env.CI ? [['line'], ['html', { open: 'never' }]] : 'list',
   use: {
     baseURL: 'http://127.0.0.1:8137',
     trace: 'on-first-retry',
